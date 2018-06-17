91a19b4f2fabbaad6f9701841264ac443adb3f0b
Victor Shenderovich <victor.shenderovich@gmail.com>
2018-06-17 12:14:16 +0300

    Merge step8. Implemented hive section viewing and editing in front end.

--- KatlaSport.Services/ProductManagement/ProductCategoryService.cs
+++ KatlaSport.Services/ProductManagement/ProductCategoryService.cs
@@ -39,8 +39,6 @@ namespace KatlaSport.Services.ProductManagement
                 c.ProductCount = await _context.Products.Where(p => p.CategoryId == c.Id).CountAsync();
             }
 
-            // TODO STEP 3 - Add code for setting "ProductCount" property for each category here.
-
             return categories;
         }
 
